4c70aeda3263330c4eb1a67e08cfce537a6425c1
Robert Garrett <garretrc@miamioh.edu>
2018-07-06 11:22:21 -0400

    Add files via upload

---
+++ host/bikes.R
@@ -0,0 +1,12 @@
+####Bikes Wrangling####
+library(dplyr)
+
+campus=read.csv("http://garretrc.github.io/host/BikeRack_Survey_data.csv",stringsAsFactors = F)
+downtown=read.csv("http://garretrc.github.io/host/downtownracks.csv",stringsAsFactors = F)
+
+oxford_bikes = dplyr::bind_rows(
+  campus %>% dplyr::select(x,y,name=Building),
+  downtown %>% dplyr::select(x=POINT_X,y=POINT_Y,name=Rack_Name)
+)
+
+write.csv(oxford_bikes,file="oxford_bikes.csv",row.names = F)
