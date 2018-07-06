ff5694f2e71cfa543dc961c1610b284e6cbbfd1f
Robert Garrett <garretrc@miamioh.edu>
2018-07-06 15:25:50 -0400

    Add files via upload

--- host/bikes.R
+++ host/bikes.R
@@ -7,6 +7,6 @@ downtown=read.csv("http://garretrc.github.io/host/downtownracks.csv",stringsAsFa
 oxford_bikes = dplyr::bind_rows(
   campus %>% dplyr::select(x,y,name=Building),
   downtown %>% dplyr::select(x=POINT_X,y=POINT_Y,name=Rack_Name)
-)
+) %>% distinct(x,y,.keep_all=T)
 
 write.csv(oxford_bikes,file="oxford_bikes.csv",row.names = F)
